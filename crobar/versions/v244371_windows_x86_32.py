be86284c1b42e8905ae8e5ed57251b5d2509a0f0
apple1417 <theapple1417@gmail.com>
2019-08-26 12:26:55 +1200

    Added Windows versions of the patches

--- crobar/versions/v244371_windows_x86_32.py
+++ crobar/versions/v244371_windows_x86_32.py
@@ -12,15 +12,14 @@ class TalosVersion_v244371_windows_x86_32(BaseTalosVersion):
     def get_version_identifier(cls) -> Tuple[int, bytes]:
         """Returns an (address, bytes) tuple uniquely identifying this build."""
         return (
-            0x01115f38,
+            0x01515f38,
             b"$Version: Talos_PC_distro; Talos-Windows-Final; 244371 2015-07-23 19:11:28 @builder14; Win32-Static-Final-Default$",)
 
     def patch_enable_esga(self) -> bool:
         """PATCH: Stops prjStartNewTalosGame() from scrubbing out the gam_esgaStartAs variable."""
-        # MOV dword ptr [EStartGameAs_09e9084c],0x0
         return self.patch_memory(
-            addr=0x00373a1f,
-            old=bytes([0x89, 0x35]) + self.pack_relative_addr(0x011d6d98),
+            addr=0x00773a1f,
+            old=bytes([0x89, 0x35, 0x98, 0x6d, 0x5d, 0x01]),
             new=bytes([0x90, 0x90, 0x90, 0x90, 0x90, 0x90]),
         )
 
@@ -28,45 +27,36 @@ class TalosVersion_v244371_windows_x86_32(BaseTalosVersion):
         """PATCH: Allows voting for any map regardless of game mode."""
         patches_applied: List[bool] = []
 
-        raise NotImplementedError()
         patches_applied.append(self.patch_memory(
-            addr=0x089387b2,
-            old=bytes([0xe8, 0xd9, 0xcc, 0xc1, 0xff, 0x85, 0xc0, 0x75, 0x5d]),
-            new=bytes([0xe8, 0xd9, 0xcc, 0xc1, 0xff, 0x85, 0xc0, 0xeb, 0x5d]),
+            addr=0x00915282,
+            old=bytes([0x74, 0x58]),
+            new=bytes([0x90, 0x90]),
         ))
 
         patches_applied.append(self.patch_memory(
-            addr=0x08939b2c,
-            old=bytes([0xe8, 0x5f, 0xb9, 0xc1, 0xff, 0x85, 0xc0, 0x75, 0x63]),
-            new=bytes([0xe8, 0x5f, 0xb9, 0xc1, 0xff, 0x85, 0xc0, 0xeb, 0x63]),
+            addr=0x00565c7e,
+            old=bytes([0xe8, 0x5d, 0xe3, 0x9c, 0x00]),
+            new=bytes([0xb8, 0x01, 0x00, 0x00, 0x00]),
+        ))
+
+        patches_applied.append(self.patch_memory(
+            addr=0x00565cb0,
+            old=bytes([0x0f, 0x85, 0xc7, 0x00, 0x00, 0x00]),
+            new=bytes([0x90, 0x90, 0x90, 0x90, 0x90, 0x90]),
         ))
 
         return any(patches_applied)
 
-    def patch_crash_on_nexus_0001(self) -> bool:
-        """PATCH: WIP"""
-
-        return False
-
-        raise NotImplementedError()
-        return self.patch_memory(
-            addr=0x08a47b15,
-            old=bytes([0xe8, 0x86, 0x50, 0xa2, 0x00]),
-            new=bytes([0x90, 0x90, 0x90, 0x90, 0x90]),
-        )
-
     def patch_upgrade_singleplayer(self) -> bool:
         """PATCH: Upgrade the SinglePlayer mode to a multiplayer mode."""
         patches_applied: List[bool] = []
-
-        raise NotImplementedError()
 
         game_mode_base: int
         game_mode_count: int
         game_mode_base, game_mode_count, = struct.unpack(
             "<II",
             self._debug_interface.read_memory(
-                addr=0x09e90fb8,
+                addr=0x0156e150,
                 length=0x8))
 
         # Find the SinglePlayer game mode
@@ -101,4 +91,19 @@ class TalosVersion_v244371_windows_x86_32(BaseTalosVersion):
 
         return any(patches_applied)
 
+    def patch_ignore_pure_mode(self) -> bool:
+        patches_applied: List[bool] = []
 
+        patches_applied.append(self.patch_memory(
+            addr=0x00F505F4,
+            old=bytes([0x74]),
+            new=bytes([0xeb]),
+        ))
+
+        patches_applied.append(self.patch_memory(
+            addr=0x00F5088D,
+            old=bytes([0x74]),
+            new=bytes([0xeb]),
+        ))
+
+        return any(patches_applied)
